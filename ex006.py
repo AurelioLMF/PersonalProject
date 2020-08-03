81c0da41336dc54bb3ba62ea6424c48dd4870041
Aurelio L. M. Fagundes <60241035+AurelioLMF@users.noreply.github.com>
2020-08-03 20:07:26 -0300

    Add files via upload
    Crie um programa que leia um número e mostre o seu dobro, triplo e sua raíz quadrada.

---
+++ ex006.py
@@ -0,0 +1,4 @@
+#Crie um programa que leia um número e mostre o seu dobro, triplo e raíz quadrada.
+n = int(input('Digite um número: '))
+print(f'O dobro de {n} vale {n*2}.')
+print(f'O triplo de {n} vale {n*3}\nA raiz quadrada de {n} é igual a {n**(1/2):.2f}')
