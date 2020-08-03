aa237e10ca84f8646ea0a7ea373062e083d4c4ab
Aurelio L. M. Fagundes <60241035+AurelioLMF@users.noreply.github.com>
2020-08-03 20:37:01 -0300

    Add files via upload
    Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.

---
+++ ex046.py
@@ -0,0 +1,7 @@
+'''Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0,
+com uma pausa de 1 segundo entre eles.'''
+from time import sleep
+for i in range(10,-1,-1):
+    print(i)
+    sleep(0.5)
+print('Acabou')
