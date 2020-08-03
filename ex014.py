f97ba41ba4820612f0f20b4d5c22dade6cd6fc21
Aurelio L. M. Fagundes <60241035+AurelioLMF@users.noreply.github.com>
2020-08-03 20:12:30 -0300

    Add files via upload
    Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF

---
+++ ex014.py
@@ -0,0 +1,5 @@
+#Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF
+c = float(input('Informe a temperatura em ºC: '))
+f = (c * 1.8)+32
+div = 9/5
+print(f'A temperatura de {c}ºC corresponde a {f}ºF.')
