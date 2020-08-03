67ec26a8f168926e6f75620584170a19b9aded69
Aurelio L. M. Fagundes <60241035+AurelioLMF@users.noreply.github.com>
2020-08-03 20:06:34 -0300

    Add files via upload
    #Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor

---
+++ ex005.py
@@ -0,0 +1,3 @@
+#Faça um programa que leia um número inteiro e mostre ba tela o seu sucessor e seu antecessor
+n = int(input('Digite um número: '))
+print(f'Seu antecessor é {n-1} e seu sucessor é {n+1}')
