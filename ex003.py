8760db7c3b4dde56438d4cd98fbd3bd2af87cd7d
Aurelio L. M. Fagundes <60241035+AurelioLMF@users.noreply.github.com>
2020-08-03 20:04:40 -0300

    Add files via upload
    Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ ex003.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+
+soma = num1 + num2
+
+print(f'A soma dos dois números = {soma}')
