5a6f53f6ee6a856c10594c8e366e1aaffeb79819
Aurelio L. M. Fagundes <60241035+AurelioLMF@users.noreply.github.com>
2020-08-03 20:13:47 -0300

    Add files via upload
    Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
    Ex.: Digite um número: 6.127
    O número 6.127 tem a parte inteira 6.

---
+++ ex016.py
@@ -0,0 +1,6 @@
+'''Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+Ex.: Digite um número: 6.127
+O número 6.127 tem a parte inteira 6.'''
+
+num = float(input('Digite um número: '))
+print(f'O número {num}, tem a parte inteira {int(num)}')
